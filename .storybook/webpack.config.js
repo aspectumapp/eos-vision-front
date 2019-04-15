cc473fecf11845c128e712c576309b89aea237ce
Yura Chuchola <smhfanda@gmail.com>
2019-04-15 17:00:45 +0300

    Added stories. Added less packages for storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,26 +1,32 @@
 module.exports = ({ config }) => {
   config.resolve.extensions.push('.ts', '.tsx');
 
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve('babel-loader'),
-        options: {
-          presets: [['react-app', { flow: false, typescript: false }]],
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [['react-app', { flow: false, typescript: false }]],
+          },
         },
-      },
-      {
-        loader: require.resolve('ts-loader'),
-      },
-      {
-        loader: require.resolve('react-docgen-typescript-loader'),
-        options: {
-          tsconfigPath: './tsconfig.json'
+        {
+          loader: require.resolve('ts-loader'),
         },
-      },
-    ],
-  });
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+          options: {
+            tsconfigPath: './tsconfig.json'
+          },
+        },
+      ],
+    },
+    {
+      test: /\.less$/,
+      loaders: [ 'style-loader', 'css-loader', 'less-loader' ],
+    }
+  );
 
   return config;
 };
