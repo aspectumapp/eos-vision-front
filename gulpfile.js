74bdc02ce43329e858e04a2d84a05d1640c74472
Yura Chuchola <smhfanda@gmail.com>
2019-04-27 10:08:14 +0300

    Refactor

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,18 @@ const gulp = require('gulp');
 gulp.task('copy:dist', function () {
   gulp
     .src(['./src/**/*', '!./src/**/*.stories.*'])
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist/'))
+  ;
+});
+
+gulp.task('copy:semantic-ui-less', function () {
+  gulp
+    .src('./node_modules/semantic-ui-less/definitions/**/*')
+    .pipe(gulp.dest('./src/assets/semantic-custom/definitions/'))
+  ;
+
+  gulp
+    .src('./node_modules/semantic-ui-less/themes/**/*')
+    .pipe(gulp.dest('./src/assets/semantic-custom/themes/'))
+  ;
 });
